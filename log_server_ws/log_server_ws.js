584d203fb94cea488c3469aa19d5023154c0189b
U-PHACTUM\heimscmi <heimscmi@PHA1WD17.Phactum.local>
2012-10-09 12:14:49 +0200

    multiple files support added publish/subscribe pattern autoscrolling

--- log_server_ws/log_server_ws.js
+++ log_server_ws/log_server_ws.js
@@ -1,8 +1,13 @@
 var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
-  , fs = require('fs')
+  , fs = require('fs');
 
 app.listen(12345);
+
+var files = [];
+for (var i = process.argv.length - 2; i--;) {
+  files.push(process.argv[i + 2]);
+}
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
@@ -17,27 +22,86 @@ function handler (req, res) {
   });
 }
 
-var sockets = [];
-
 io.sockets.on('connection', function (socket) {
-  sockets.push(socket);
+  socket.emit('init', { message: files });
+  publisher.subscribe_for_all(socket);
   socket.on('disconnect', function() {
-    for (var i = sockets.length; i--;) {
-      if (sockets[i] == socket) {
-        sockets.splice(i, 1);
+    publisher.unsubscribe_from_all(socket);
+  });
+  socket.on('subscribe', function(data) {
+    publisher.subscribe(data.file, socket);
+  });
+
+  socket.on('unsubscribe', function(data) {
+    publisher.unsubscribe(data.file, socket);
+  });
+});
+
+var publisher = {
+  subscriptions: {},
+
+  subscribe: function(file, socket) {
+    if (this.subscriptions[file] == undefined) {
+      this.subscriptions[file] = [];
+    }
+    this.subscriptions[file].push(socket);
+  },
+
+  subscribe_for_all: function(socket) {
+    for (var i = files.length; i--; ) {
+      this.subscribe(files[i], socket);
+    }
+  },
+
+  unsubscribe: function(file, socket) {
+    for (var i = this.subscriptions[file].length; i--;) {
+      if (this.subscriptions[file][i] == socket) {
+        this.subscriptions[file].splice(i, 1);
       }
     }
-  })
-});
+  },
 
-fs.open(process.argv[2], 'r', function(err, fd) {
-    fs.watchFile(process.argv[2], function(c, p) {
-        console.log(c.size - p.size);
-        fs.createReadStream(process.argv[2], { start: p.size, end: c.size}).addListener("data", function(lines) {
-            for (var i = sockets.length; i--;) {
-                sockets[i].emit('log', {message: lines.toString()});
-            }
-        });
-    })
-});
+  unsubscribe_from_all: function(socket) {
+    for (file in this.subscriptions) {
+      this.unsubscribe(file, socket);
+    }
+  },
 
+  publish: function(fn, data) {
+    if (undefined == this.subscriptions[fn]) {
+      return;
+    }
+    for (var i = this.subscriptions[fn].length; i--;) {
+      this.subscriptions[fn][i].emit('log', data);
+    }
+  }
+}
+
+var sizes = {};
+
+for (var i = files.length; i--;) {
+  var file_name = files[i];
+  fs.stat(file_name, function(error, stat) {
+    if (error) {
+      return;
+    }
+
+    sizes[file_name] = stat.size;
+  });
+
+  fs.watch(file_name, function(event, fn) {
+    if ("change" != event) {
+      return;
+    }
+
+    fs.stat(fn, function(error, stat) {
+      var p_size = sizes[fn];
+      var c_size = stat.size;
+
+      fs.createReadStream(fn, { start: p_size > c_size ? 0 : p_size, end: c_size}).addListener("data", function(lines) {
+        publisher.publish(fn, {message: lines.toString()});
+        sizes[fn] = c_size;
+      });
+    });
+  });
+}
